e401ffba9f362e36187d83ac07588739359866ed
SirPatschiii <tofue2003@gmx.de>
2024-01-24 19:16:53 +0100

    Implemented base methods in GUI

--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,15 @@
+import tkinter as tk
+
+
 class GUI:
     def __init__(self):
         print("GUI works!")
+        self.window = tk.Tk()
+        self.gui_status = "main"
+
+    def update_gui(self):
+        # TODO hier müssen die fertigen Buttons, Labels etc. ankommen
+        pass
+
+    def get_gui_status(self):
+        return self.gui_status

22b27ef267684bc17160fd04cee221cd3e84dc29
SirPatschiii <tofue2003@gmx.de>
2024-01-26 10:20:17 +0100

    Implemented test button

--- src/Controller.py
+++ src/Controller.py
@@ -1,9 +1,4 @@
 from GUI import GUI
-from Button import Button
-from Entry import Entry
-from Label import Label
-from Spinbox import Spinbox
-from Radiobox import Radiobox
 from Cinema import Cinema
 from Movie import Movie
 from Event import Event
@@ -14,11 +9,6 @@ class Controller:
     def __init__(self):
         print("Controller works!")
         self.g_GUI = GUI()
-        self.g_Button = Button(self.g_GUI.get_window())
-        self.g_Entry = Entry()
-        self.g_Label = Label()
-        self.g_Spinbox = Spinbox()
-        self.g_Radiobox = Radiobox()
         self.g_Cinema = Cinema()
         self.g_Movie = Movie()
         self.g_Event = Event()
--- src/GUI.py
+++ src/GUI.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter as tk
 
 
@@ -11,17 +12,41 @@ class GUI:
         self.window.geometry("1000x700")
         self.window.resizable(False, False)
         self.window.title("Kinobuchungsprogramm")
+        self.update_gui("main")
         self.window.mainloop()
 
-    def update_gui(self, p_gui_status):
-        # TODO hier müssen die fertigen Buttons, Labels etc. ankommen
+    def update_gui(self, gui_status):
+        if gui_status == "main":
+            self.__update_gui_main_menu()
+        elif gui_status == "cinema":
+            self.__update_gui_cinema_menu()
+        elif gui_status == "movie":
+            self.__update_gui_movie_menu()
+        elif gui_status == "event":
+            self.__update_gui_event_menu()
+        elif gui_status == "booking":
+            self.__update_gui_booking_menu()
+        else:
+            sys.exit(-1)
+
+    def __update_gui_main_menu(self):
+        btn = tk.Button(self.window, text="Test", command="", width=10, height=2)
+        btn.place(x=50, y=50)
+
+    def __update_gui_cinema_menu(self):
+        pass
+
+    def __update_gui_movie_menu(self):
+        pass
+
+    def __update_gui_event_menu(self):
+        pass
+
+    def __update_gui_booking_menu(self):
         pass
 
     def get_gui_status(self):
         return self.gui_status
-
-    def get_window(self):
-        return self.window
 
     def set_gui_status(self, p_gui_status):
         self.gui_status = p_gui_status

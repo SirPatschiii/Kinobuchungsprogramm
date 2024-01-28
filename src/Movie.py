33b63306a6cbc468c158a1e31219300adbc988cd
grilldb97 <dustin.bellem@web.de>
2024-01-28 13:05:23 +0100

    Datenbanken erstellt und eine Testconnection in Movie erstellt

--- src/Movie.py
+++ src/Movie.py
@@ -1,3 +1,20 @@
+import sqlite3
+
+
 class Movie:
     def __init__(self):
         print("Movie works!")
+
+    @staticmethod
+    def connect_db():
+        connect = sqlite3.connect("cinemadata.db")
+        cursor_db = connect.cursor()
+
+    @staticmethod
+    def test_connect(cursor_db):
+        cursor_db.execute("SELECT * FROM movie")
+        user_data = cursor_db.fetchall()
+        print(user_data)
+
+
+

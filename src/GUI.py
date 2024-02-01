1604b464d881f5559d2bff8a64dcde7e013ec75a
SirPatschiii <tofue2003@gmx.de>
2024-02-01 15:12:46 +0100

    Implemented booking GUI

--- src/GUI.py
+++ src/GUI.py
@@ -15,15 +15,15 @@ global lbl_movie_lab3
 global btn_movie_mov1
 global btn_movie_mov2
 global btn_movie_mov3
-global txt_movie_tx1
-global txt_movie_tx2
-global txt_movie_tx3
-global scb_movie_sb1
-global scb_movie_sb2
-global scb_movie_sb3
-global fra_movie_fr1
-global fra_movie_fr2
-global fra_movie_fr3
+global txt_movie_txt1
+global txt_movie_txt2
+global txt_movie_txt3
+global scb_movie_scb1
+global scb_movie_scb2
+global scb_movie_scb3
+global fra_movie_fra1
+global fra_movie_fra2
+global fra_movie_fra3
 
 global lbl_event_lab1
 global rad_event_rad1
@@ -33,6 +33,12 @@ global btn_event_btn1
 global radio_var
 global event_lab1_var
 
+global lbl_booking_lab1
+global lbl_booking_lab2
+global lbl_booking_lab3
+global btn_booking_btn_list
+global btn_booking_btn1
+
 
 class GUI:
     def __init__(self, p_controller):
@@ -40,6 +46,9 @@ class GUI:
         self.controller = p_controller
         self.window = tk.Tk()
         self.gui_status = "main"
+
+        global btn_booking_btn_list
+        btn_booking_btn_list = []
 
     def create_gui(self):
         self.window.geometry("1000x700")
@@ -118,50 +127,50 @@ class GUI:
                                        movie_title_lbl), width=50, height=5)
         btn_movie_mov3.place(x=150, y=520)
 
-        global txt_movie_tx1
-        global txt_movie_tx2
-        global txt_movie_tx3
-        global scb_movie_sb1
-        global scb_movie_sb2
-        global scb_movie_sb3
-        global fra_movie_fr1
-        global fra_movie_fr2
-        global fra_movie_fr3
+        global txt_movie_txt1
+        global txt_movie_txt2
+        global txt_movie_txt3
+        global scb_movie_scb1
+        global scb_movie_scb2
+        global scb_movie_scb3
+        global fra_movie_fra1
+        global fra_movie_fra2
+        global fra_movie_fra3
 
         movie_descriptions = self.controller.show_movie_description()
 
-        fra_movie_fr1 = tk.Frame(self.window, width=55, height=5)
-        fra_movie_fr1.place(x=550, y=70)
-        txt_movie_tx1 = tk.Text(fra_movie_fr1, width=50, height=5)
-        scb_movie_sb1 = tk.Scrollbar(fra_movie_fr1)
-        txt_movie_tx1.pack(side=tkinter.constants.LEFT, fill=tkinter.constants.Y)
-        scb_movie_sb1.pack(side=tkinter.constants.RIGHT, fill=tkinter.constants.Y)
-        scb_movie_sb1.config(command=txt_movie_tx1.yview)
-        txt_movie_tx1.config(yscrollcommand=scb_movie_sb1.set)
-        txt_movie_tx1.insert(tkinter.END, movie_descriptions[0])
-        txt_movie_tx1.config(state=tk.DISABLED)
+        fra_movie_fra1 = tk.Frame(self.window, width=55, height=5)
+        fra_movie_fra1.place(x=550, y=70)
+        txt_movie_txt1 = tk.Text(fra_movie_fra1, width=50, height=5)
+        scb_movie_scb1 = tk.Scrollbar(fra_movie_fra1)
+        txt_movie_txt1.pack(side=tkinter.constants.LEFT, fill=tkinter.constants.Y)
+        scb_movie_scb1.pack(side=tkinter.constants.RIGHT, fill=tkinter.constants.Y)
+        scb_movie_scb1.config(command=txt_movie_txt1.yview)
+        txt_movie_txt1.config(yscrollcommand=scb_movie_scb1.set)
+        txt_movie_txt1.insert(tkinter.END, movie_descriptions[0])
+        txt_movie_txt1.config(state=tk.DISABLED)
 
-        fra_movie_fr2 = tk.Frame(self.window, width=55, height=5)
-        fra_movie_fr2.place(x=550, y=290)
-        txt_movie_tx2 = tk.Text(fra_movie_fr2, width=50, height=5)
-        scb_movie_sb2 = tk.Scrollbar(fra_movie_fr2)
-        txt_movie_tx2.pack(side=tkinter.constants.LEFT, fill=tkinter.constants.Y)
-        scb_movie_sb2.pack(side=tkinter.constants.RIGHT, fill=tkinter.constants.Y)
-        scb_movie_sb2.config(command=txt_movie_tx2.yview)
-        txt_movie_tx2.config(yscrollcommand=scb_movie_sb2.set)
-        txt_movie_tx2.insert(tkinter.END, movie_descriptions[1])
-        txt_movie_tx2.config(state=tk.DISABLED)
+        fra_movie_fra2 = tk.Frame(self.window, width=55, height=5)
+        fra_movie_fra2.place(x=550, y=290)
+        txt_movie_txt2 = tk.Text(fra_movie_fra2, width=50, height=5)
+        scb_movie_scb2 = tk.Scrollbar(fra_movie_fra2)
+        txt_movie_txt2.pack(side=tkinter.constants.LEFT, fill=tkinter.constants.Y)
+        scb_movie_scb2.pack(side=tkinter.constants.RIGHT, fill=tkinter.constants.Y)
+        scb_movie_scb2.config(command=txt_movie_txt2.yview)
+        txt_movie_txt2.config(yscrollcommand=scb_movie_scb2.set)
+        txt_movie_txt2.insert(tkinter.END, movie_descriptions[1])
+        txt_movie_txt2.config(state=tk.DISABLED)
 
-        fra_movie_fr3 = tk.Frame(self.window, width=55, height=5)
-        fra_movie_fr3.place(x=550, y=520)
-        txt_movie_tx3 = tk.Text(fra_movie_fr3, width=50, height=5)
-        scb_movie_sb3 = tk.Scrollbar(fra_movie_fr3)
-        txt_movie_tx3.pack(side=tkinter.constants.LEFT, fill=tk.constants.Y)
-        scb_movie_sb3.pack(side=tkinter.constants.LEFT, fill=tk.constants.Y)
-        scb_movie_sb3.config(command=txt_movie_tx3.yview)
-        txt_movie_tx3.config(yscrollcommand=scb_movie_sb3.set)
-        txt_movie_tx3.insert(tkinter.END, movie_descriptions[2])
-        txt_movie_tx3.config(state=tk.DISABLED)
+        fra_movie_fra3 = tk.Frame(self.window, width=55, height=5)
+        fra_movie_fra3.place(x=550, y=520)
+        txt_movie_txt3 = tk.Text(fra_movie_fra3, width=50, height=5)
+        scb_movie_scb3 = tk.Scrollbar(fra_movie_fra3)
+        txt_movie_txt3.pack(side=tkinter.constants.LEFT, fill=tk.constants.Y)
+        scb_movie_scb3.pack(side=tkinter.constants.LEFT, fill=tk.constants.Y)
+        scb_movie_scb3.config(command=txt_movie_txt3.yview)
+        txt_movie_txt3.config(yscrollcommand=scb_movie_scb3.set)
+        txt_movie_txt3.insert(tkinter.END, movie_descriptions[2])
+        txt_movie_txt3.config(state=tk.DISABLED)
 
     def __update_gui_event_menu(self):
         self.__clear_gui()
@@ -177,7 +186,7 @@ class GUI:
         self.radio_var = tk.StringVar(value="Option 1")
         event_select = self.controller.show_events()
         lbl_event_lab1 = tk.Label(self.window, text=event_lab1_var, font=("Arial", 14), width=50, height=2, anchor="w")
-        lbl_event_lab1.place(x=80, y=80)
+        lbl_event_lab1.place(x=80, y=60)
         rad_event_rad1 = tk.Radiobutton(self.window, text=event_select[0], variable=self.radio_var,
                                         value="Option 1", font=("Arial", 12))
         rad_event_rad2 = tk.Radiobutton(self.window, text=event_select[1], variable=self.radio_var,
@@ -195,6 +204,29 @@ class GUI:
     def __update_gui_booking_menu(self):
         self.__clear_gui()
 
+        global lbl_booking_lab1
+        global lbl_booking_lab2
+        global lbl_booking_lab3
+        global btn_booking_btn_list
+        global btn_booking_btn1
+
+        lbl_booking_lab1 = tk.Label(self.window, text="Filmtitel", font=("Arial", 14), width=50, height=2, anchor="w")
+        lbl_booking_lab1.place(x=80, y=60)
+        lbl_booking_lab2 = tk.Label(self.window, text="Kinoname", font=("Arial", 14), width=50, height=2, anchor="w")
+        lbl_booking_lab2.place(x=80, y=110)
+        lbl_booking_lab3 = tk.Label(self.window, text="Datum/Uhrzeit", font=("Arial", 14), width=50, height=2,
+                                    anchor="e")
+        lbl_booking_lab3.place(x=350, y=60)
+
+        for i in range(4):
+            for j in range(5):
+                button = tk.Button(self.window, text=(i * 5) + (j + 1), font=("Arial", 14), width=7, height=2)
+                button.place(x=250 + (100 * j), y=240 + (80 * i))
+                btn_booking_btn_list.append(button)
+
+        btn_booking_btn1 = tk.Button(self.window, text="Weiter", command=None, width=10, height=2)
+        btn_booking_btn1.place(x=700, y=640)
+
     def __clear_gui(self):
         # This method destroys all elements on the current GUI to ensure only the correct widgets for the current
         # GUI state get displayed
@@ -209,21 +241,26 @@ class GUI:
         global btn_movie_mov1
         global btn_movie_mov2
         global btn_movie_mov3
-        global txt_movie_tx1
-        global txt_movie_tx2
-        global txt_movie_tx3
-        global scb_movie_sb1
-        global scb_movie_sb2
-        global scb_movie_sb3
-        global fra_movie_fr1
-        global fra_movie_fr2
-        global fra_movie_fr3
+        global txt_movie_txt1
+        global txt_movie_txt2
+        global txt_movie_txt3
+        global scb_movie_scb1
+        global scb_movie_scb2
+        global scb_movie_scb3
+        global fra_movie_fra1
+        global fra_movie_fra2
+        global fra_movie_fra3
 
         global lbl_event_lab1
         global rad_event_rad1
         global rad_event_rad2
         global rad_event_rad3
         global btn_event_btn1
+
+        global lbl_booking_lab1
+        global lbl_booking_lab2
+        global lbl_booking_lab3
+        global btn_booking_btn1
 
         try:
             btn_main_cinema.destroy()
@@ -236,23 +273,36 @@ class GUI:
             btn_movie_mov1.destroy()
             btn_movie_mov2.destroy()
             btn_movie_mov3.destroy()
-            txt_movie_tx1.destroy()
-            txt_movie_tx2.destroy()
-            txt_movie_tx3.destroy()
-            scb_movie_sb1.destroy()
-            scb_movie_sb2.destroy()
-            scb_movie_sb3.destroy()
-            fra_movie_fr1.destroy()
-            fra_movie_fr2.destroy()
-            fra_movie_fr3.destroy()
+            txt_movie_txt1.destroy()
+            txt_movie_txt2.destroy()
+            txt_movie_txt3.destroy()
+            scb_movie_scb1.destroy()
+            scb_movie_scb2.destroy()
+            scb_movie_scb3.destroy()
+            fra_movie_fra1.destroy()
+            fra_movie_fra2.destroy()
+            fra_movie_fra3.destroy()
 
             lbl_event_lab1.destroy()
             rad_event_rad1.destroy()
             rad_event_rad2.destroy()
             rad_event_rad3.destroy()
             btn_event_btn1.destroy()
+
+            lbl_booking_lab1.destroy()
+            lbl_booking_lab2.destroy()
+            lbl_booking_lab3.destroy()
+            btn_booking_btn1.destroy()
         except:
             pass
+
+        global btn_booking_btn_list
+
+        for button in btn_booking_btn_list:
+            try:
+                button.destroy()
+            except:
+                pass
 
     def get_selected_radiobutton(self):
         return self.radio_var.get()
